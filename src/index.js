e02cd38a544cb9d6a8e10d77f358aa557d77cd9a
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2021-07-10 17:50:14 -0300

    versao inicial do projeto - apenas layout

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculadora from './main/Calculator';
+
+ReactDOM.render(
+  <div>
+    <h1>Calculadora</h1>
+    <Calculadora />
+  </div>,
+  document.getElementById('root')
+);
